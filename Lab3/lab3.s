ff4f3e751c7fb6aba9d30e1578a3c329ce544053
Ronnie Mayberry <ronnielmayberry@gmail.com>
2021-09-22 14:55:48 -0500

    Update lab3.s

--- Lab3/lab3.s
+++ Lab3/lab3.s
@@ -1,9 +1,10 @@
-IOCON_P0_13	EQU 0x4002C034 ; LED 2 P0[13] IOCON_P0_13 R/W 0x1B0 0x4002C034 A
+IOCON_P0_13		EQU 0x4002C034 ; LED 2 P0[13] IOCON_P0_13 R/W 0x1B0 0x4002C034 A
 IOCON_P1_13 	EQU 0x4002C0B4 ; LED 3 P1[13] IOCON_P1_13 R/W 0x030 0x4002C0B4 D
 IOCON_P1_18 	EQU 0x4002C0C8 ; LED 1 P1[18] IOCON_P1_18 R/W 0x030 0x4002C0C8 D
-IOCON_P2_19	EQU 0x4002C14C ; LED 4 P2[19] IOCON_P2_19 R/W 0x030 0x4002C14C D
+IOCON_P2_19		EQU 0x4002C14C ; LED 4 P2[19] IOCON_P2_19 R/W 0x030 0x4002C14C D
 	
-IOCON_P1_2 	EQU 0x4002C088 ; external LED - PIN 30
+IOCON_P0_5		EQU 0x4002C014	; external switch
+IOCON_P1_2 		EQU 0x4002C088 ; external LED - PIN 30 POSITIVE LOGIC
 IOCON_P2_10 	EQU 0x4002C128 ; user switch - PIN 23
 
 ; This is essentially Table 94 from UM10562
@@ -21,6 +22,7 @@ CLR_REG_OFFSET 	EQU 0x1C
 	
 ; End Table 94
 bit2		EQU (1 <<  2)
+bit5		EQU (1 <<  5)
 bit10		EQU (1 << 10)
 bit13 		EQU (1 << 13) ; 0x00002000
 bit18 		EQU (1 << 18) ; 0x00040000
@@ -30,10 +32,7 @@ bit19		EQU (1 << 19)
 		ENTRY
 		EXPORT 			__main
 __main
-		; port 2[10] LED
-		; setup the, IOCON, I/O configuration - default is 0x30, setup to 0x00
-		; setup for GPIO and inactive and no hysteresis
-		
+		; setup ext led on port1_5
 		LDR 			R1, =IOCON_P1_2 ; ping 30 extLED
 		LDR 			R2, =0x00000000
 		STR 			R2,[R1,#0x00]
@@ -46,40 +45,66 @@ __main
 		LDR 			R3,=0x00000000 ; MSK register setting
 		STR 			R3,[R1,#MSK_REG_OFFSET] ; set MASK bits 
 		
-		MOV			R12, #1
+		LDR 			R1,=LPC4088QSB_P1 ; pointer to base reg of port1 
+		LDR 			R3,=bit2 ; MSK register setting
+		STR 			R3,[R1,#CLR_REG_OFFSET] ; set MASK bits 
 		
-read		
-		LDR 			R0,=LPC4088QSB_P2 ; pointer to base reg of port2
-		LDR			R3,[R0,#PIN_REG_OFFSET]
-		LDR 			R4, bit10 
-		ANDS			R4, R3	; set Z flag (0 indicates pressed)
-		
-		BNE read		; if ANDS => 1 (not pressed)
-		
-		; pressed
-		CMP			R12, #1	; is LED currently on (R12 = 1) or off (R12 = 0)
-		BEQ turnoff		; if on (R12=1), turn off LED '1'
-		BNE turnon		; if off (R12=0), turn on LED '0'
-	
-turnon
-		MOV			R12, #1 ; LED is now on
-
-		LDR 			R1,=LPC4088QSB_P2 ; pointer to base reg of port2
-		LDR 			R3,=bit10 ; 
-		STR 			R3,[R1,#SET_REG_OFFSET] ; set CLEAR bits - This turns OFF the LED
 		bl delay
 		
-		b read		
-
-turnoff
-		MOV			R12, #0 ; led is now off
-
-		LDR 			R1,=LPC4088QSB_P2 ; pointer to base reg of port2
-		LDR 			R3,=bit10 ; 
-		STR 			R3,[R1,#CLR_REG_OFFSET] ; set CLEAR bits - This turns OFF the LED
-		bl delay	
+		MOV 			R12,#0
 		
 		b read
+		
+read
+		; setup ext led on port1_5
+		LDR 			R1, =IOCON_P0_5 ; ping 30 extLED
+		LDR 			R2, =0x00000000
+		STR 			R2,[R1,#0x00]
+		
+		LDR 			R1,=LPC4088QSB_P0 ; pointer to base reg of port1
+		LDR 			R3,=bit5 ; set LED 1 port pin to output
+		STR 			R3,[R1,#DIR_REG_OFFSET] ; set DIRECTION bits
+		
+		LDR 			R1,=LPC4088QSB_P0 ; pointer to base reg of port1 
+		LDR 			R3,=0x00000000 ; MSK register setting
+		STR 			R3,[R1,#MSK_REG_OFFSET] ; set MASK bits 
+		
+		LDR 			R1,=LPC4088QSB_P0 ; pointer to base reg of port1 
+		LDR 			R3,=0x00000000 ; MSK register setting
+		LDR 			R3,[R1,#PIN_REG_OFFSET] ; set MASK bits 			
+		
+		CMP			R3, #bit5
+		BEQ 			pressed
+		
+		b read
+		
+pressed
+		CMP 			R12,#0 ; 0 is off
+		BEQ			turnon
+		BNE			turnoff
+		
+turnon
+		LDR 			R1,=LPC4088QSB_P0
+		LDR			R3,=bit5
+		STR			R3,[R1,#SET_REG_OFFSET]
+
+		MOV			R12,#1
+		
+		bl delay
+		
+		b read
+		
+turnoff
+		LDR 			R1,=LPC4088QSB_P0
+		LDR			R3,=bit5
+		STR			R3,[R1,#CLR_REG_OFFSET]
+
+		MOV			R12,#0
+
+		bl delay
+
+		b read
+		
 stp
 		b stp
  
