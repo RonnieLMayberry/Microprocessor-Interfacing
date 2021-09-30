37bb48e4425d0b54a4c85a4577d495d899d228cd
Ronnie Mayberry <ronnielmayberry@gmail.com>
2021-09-29 21:28:46 -0500

    clean up

--- Lab5a/main.cpp
+++ Lab5a/main.cpp
@@ -21,7 +21,11 @@ DigitalOut myData(p30);
 // 8 - 0, 1, 2, 3, 4, 5, 6
 // 9 - 0, 1, 2, 5, 6
 
-void cycle(int num) {
+// take in the data value, and the number of cycles
+void cycle(int data, int num) {
+	// set data value
+	myData = data;
+	// go through 'num' cycles with set data value
 	for (int i = 1; i <= num; i++) {
 		myCLK = 1;
 		myCLK = 0;
@@ -29,270 +33,155 @@ void cycle(int num) {
 }
 
 int main() {
+	// reset !CLR by setting to 1, 0, then 1
 	myCLR = 1;
 	wait(.1);
 	myCLR = 0;
 	wait(.1);
 	myCLR = 1;
 		
+	// ensure clock begins at 0
 	myCLK = 0;
 	
+	// cycle( data (0 / 1), # of cycles )
+	// data = 0 ... ON
+	// data = 1 ... OFF
 	while(true) {
-		// 0
-		myData = 1;
-		cycle(2);
-		
-		myData = 0;
-		cycle(6);
+		// '0' ... 1 1 0 0 0 0 0 0
+		cycle(1, 2);
+		cycle(0, 6);
 		
 		wait(.5);
 		
-		// 1
-		myData = 1;
-		cycle(5);
-		
-		myData = 0;
-		cycle(2);
-		
-		myData = 1;
-		cycle(1);
+		// '1' ... 1 1 1 1 1 0 0 1
+		cycle(1, 5);
+		cycle(0, 2);
+		cycle(1, 1);
 		
 		wait(.5);
 		
-		// 2
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(1);
-		
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(2);
-		
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(2);
+		// '2' ... 1 0 1 0 0 1 0 0
+		cycle(1, 1);
+		cycle(0, 1);
+		cycle(1, 1);
+		cycle(0, 2);
+		cycle(1, 1);
+		cycle(0, 2);
 
 		wait(.5);
 		
-		// 3
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(1);
-		
-		myData = 1;
-		cycle(2);
-		
-		myData = 0;
-		cycle(4);
+		// '3' ... 1 0 1 1 0 0 0 0
+		cycle(1, 1);
+		cycle(0, 1);
+		cycle(1, 2);
+		cycle(0, 4);
 
 		wait(.5);
 		
-		// 4
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(2);
-		
-		myData = 1;
-		cycle(2);
-		
-		myData = 0;
-		cycle(2);
-		
-		myData = 1;
-		cycle(1);
+		// '4' ... 1 0 0 1 1 0 0 1
+		cycle(1, 1);
+		cycle(0, 2);
+		cycle(1, 2);
+		cycle(0, 2);
+		cycle(1, 1);
 
 		wait(.5);
 		
-		// 5
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(2);
-		
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(2);
-		
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(1);
+		// '5' ... 1 0 0 1 0 0 1 0
+		cycle(1, 1);
+		cycle(0, 2);
+		cycle(1, 1);
+		cycle(0, 2);
+		cycle(1, 1);
+		cycle(0, 1);
 
 		wait(.5);
 		
-		// 6
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(5);
-		
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(1);
+		// '6' ... 1 0 0 0 0 0 1 0
+		cycle(1, 1);
+		cycle(0, 5);
+		cycle(1, 1);
+		cycle(0, 1);
 
 		wait(.5);
 		
-		// 7
-		myData = 1;
-		cycle(1);
-		
-		myData = 1;
-		cycle(4);
-		
-		myData = 0;
-		cycle(3);
+		// '7' ... 1 1 1 1 1 0 0 0
+		cycle(1, 5);
+		cycle(0, 3);
 
 		wait(.5);
 		
-		// 8
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(7);
+		// '8' ... 1 0 0 0 0 0 0 0
+		cycle(1, 1);
+		cycle(0, 7);
 
 		wait(.5);
 		
-		// 9
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(2);
-		
-		myData = 1;
-		cycle(2);
-		
-		myData = 0;
-		cycle(3);
+		// '9' ... 1 0 0 1 1 0 0 0
+		cycle(1, 1);
+		cycle(0, 2);
+		cycle(1, 2);
+		cycle(0, 3);
 
 		wait(.5);
 		
-		
-		
-		
-		// A
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(3);
-		
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(3);
+		// 'A' ... 1 0 0 0 1 0 0 0
+		cycle(1, 1);
+		cycle(0, 3);
+		cycle(1, 1);
+		cycle(0, 3);
 
 		wait(.5);
 		
-		// b
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(5);
-		
-		myData = 1;
-		cycle(2);
+		// 'b' ... 1 0 0 0 0 0 1 1
+		cycle(1, 1);
+		cycle(0, 5);
+		cycle(1, 2);
 
 		wait(.5);
 		
-		// C
-		myData = 1;
-		cycle(1);
-		
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(3);
-		
-		myData = 1;
-		cycle(2);
-		
-		myData = 0;
-		cycle(1);
+		// 'C' ... 1 1 0 0 0 1 1 0
+		cycle(1, 2);
+		cycle(0, 3);
+		cycle(1, 2);
+		cycle(0, 1);
 
 		wait(.5);
 		
-		// d
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(1);
-		
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(4);
-		
-		myData = 1;
-		cycle(1);
+		// 'd' ... 1 0 1 0 0 0 0 1
+		cycle(1, 1);
+		cycle(0, 1);
+		cycle(1, 1);
+		cycle(0, 4);
+		cycle(1, 1);
 
 		wait(.5);
 		
-		// E
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(4);
-		
-		myData = 1;
-		cycle(2);
-		
-		myData = 0;
-		cycle(1);
+		// 'E' ... 1 0 0 0 0 1 1 0
+		cycle(1, 1);
+		cycle(0, 4);
+		cycle(1, 2);
+		cycle(0, 1);
 
 		wait(.5);
 		
-		// F
-		myData = 1;
-		cycle(1);
-		
-		myData = 0;
-		cycle(3);
-		
-		myData = 1;
-		cycle(3);
-		
-		myData = 0;
-		cycle(1);
+		// 'F' ... 1 0 0 0 1 1 1 0
+		cycle(1, 1);
+		cycle(0, 3);
+		cycle(1, 3);
+		cycle(0, 1);
 
 		wait(.5);
 		
-		// .
-		myData = 0;
-		cycle(1);
-		
-		myData = 1;
-		cycle(7);
+		// '.' ... 0 1 1 1 1 1 1 1
+		cycle(0, 1);
+		cycle(1, 7);
 
 		wait(.5);
 		
+		// iterate through each LED
 		for (int i = 0; i < 8; i++) {
-			myData = i;
-			cycle(1);
+			cycle(i, 1);
 			wait(.5);
 		}
 	}
